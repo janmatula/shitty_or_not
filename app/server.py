e2d79a16f1e5b797a1cdb71e99520a992839bdc9
Jan Matula <jan.matula@ceitec.local>
2020-02-26 13:26:31 +0100

    size fix

--- app/server.py
+++ app/server.py
@@ -61,9 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     shape = img.shape
-    if (shape[0] + shape[1])>1200:
-        img = img.resize(torch.Size([shape[0], int(shape[1]/2), int(shape[2]/2)]))
-    if (shape[0] + shape[1])>1200:
+    while (shape[0] + shape[1])>1300:
         img = img.resize(torch.Size([shape[0], int(shape[1]/2), int(shape[2]/2)]))
     prediction = learn.predict(img)[0]
 
