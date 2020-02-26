9fd9bbb9e3e3440e371ebccd00cb220427778431
Jan Matula <jan.matula@ceitec.local>
2020-02-26 12:20:50 +0100

    download key

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1-1PuIbdDimmb8LzeMQKhQRtkIqTbEooW/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-1PuIbdDimmb8LzeMQKhQRtkIqTbEooW'
 export_file_name = 'export.pkl'
 
 classes = ['Sexy', 'Shitty', 'Is that food?']
